00854ba5f939224ebc6006f9de4fe63a3031b73c
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-09 05:20:39 +0200

    using expect instead of (if condition )

--- test/math.test.js
+++ test/math.test.js
@@ -1,3 +1,4 @@
+const { expectCt } = require('helmet')
 const {calcTip}=require('./math')
 test('hello world',()=>{
 
@@ -9,5 +10,6 @@ test('fail',()=>{
 
 test('calc fuction return the the total money',()=>{
     const total=calcTip(10,.3)
-    if(total!==13)throw Error('error in calc the tip')
+    // if(total!==13)throw Error('error in calc the tip')
+    expect(total).toBe(13)
 })
