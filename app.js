9e4c3dc7d16f29641cb24967589298695ccc4954
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-17 08:49:34 +0200

    add to heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const rateLimit=require('express-rate-limit')
 
 // swagger 
 const swaggerUI=require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+const swaggerDocument = require('./swagger.yml')
 app.use('/api/v1/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
